71f5823d765a9c05149ecef89d469bf5044967fd
Jack T <jackmt9@gmail.com>
2020-02-24 23:15:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 def get_user_input
   gets.chomp
+  binding.pry
 end
 
 def prompt_user
@@ -21,5 +22,4 @@ end
 def runner
   prompt_user
   selection(get_user_input)
-binding.pry
 end
